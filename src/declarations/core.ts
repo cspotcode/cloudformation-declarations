f1fd85b331e7967b74bf747a3464f711e051aa46
Andrew Bradley <cspotcode@gmail.com>
2018-08-31 12:19:40 -0400

    Fixes #5

--- src/declarations/core.ts
+++ src/declarations/core.ts
@@ -4,7 +4,8 @@ import * as Functions from './functions';
 /* Non-project-specific TypeScript helpers and magic */
 export type TODO = any;
 export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type StringKeyOf<T> = Extract<keyof T, string>;
+export type Omit<T, K extends keyof T> = Pick<T, Diff<StringKeyOf<T>, K>>;
 
 /**
  * Identity function for declaring types within deeply nested object literals.
