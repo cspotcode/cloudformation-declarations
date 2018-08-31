53356060d1f6e9e8af6d8a106f7f628f898989ff
Andrew Bradley <cspotcode@gmail.com>
2018-08-31 13:52:48 -0400

    Fix core generics again for TS2.9+

--- src/declarations/core.ts
+++ src/declarations/core.ts
@@ -3,9 +3,7 @@ import * as Functions from './functions';
 
 /* Non-project-specific TypeScript helpers and magic */
 export type TODO = any;
-export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
-export type StringKeyOf<T> = Extract<keyof T, string>;
-export type Omit<T, K extends keyof T> = Pick<T, Diff<StringKeyOf<T>, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 /**
  * Identity function for declaring types within deeply nested object literals.
